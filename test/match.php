6f6487aa57a00f38bbc8fe8706e94e4503baa27c
bim-g <ibmussafb@gmail.com>
2021-03-18 10:17:52 +0200

    fx return type error

--- test/match.php
+++ test/match.php
@@ -2,7 +2,9 @@
 $check=[
     "password"=>[
         "required"=>true,
-        "matches"=> "n_password"
+        "matches"=> "n_password",
+        "min"=>-1,
+        "max"=>3
     ]
     ];
 $valid->check($source, $check);
