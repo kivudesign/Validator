5496d99442af107d15c853174e17cc5c602ce45a
bim-g <ibmussafb@gmail.com>
2022-01-02 13:45:19 +0200

    [UPD] update translation for vnumber

--- lang/fr/language.php
+++ lang/fr/language.php
@@ -1,10 +1,16 @@
 <?php
 $language=[
-    "`%s` should have minimum of `%s` caracters"=>"`%s` doit avoir un minimum de `%s` caracteres",
-    "`%s` should have maximum of `%s` caracters"=>"`%s` doit avoir un maximum de `%s` caracteres",
+    "`%s` should have minimum of `%s` characters"=>"`%s` doit avoir un minimum de `%s` caractèress",
+    "`%s` should have maximum of `%s` characters"=>"`%s` doit avoir un maximum de `%s` caractèress",
     "`%s` this should be an email"=>"`%s` doit etre un email",
     "`%s` this should be a link(url)"=>"`%s` doit avoir un lien(url) ",
     "`%s` should match %s"=>"`%s` doit correpondre a `%s`",
     "`%s` is required"=>"`%s` est obligatoire",
-    "`%s` should be a string"=>"`%s` doit est une chaine de caractere",
+    "`%s` should be a string"=>"`%s` doit est une chaine de caractères",
+    "`%s` should be greater than  `%s`"=>"`%s` devrait être supérieur à `%s`",
+    "`%s` should be less than  `%s`"=>"`%s` devrait être inférieur à `%s`",
+    "`%s` should be a positive number"=>"`%s` devrait être un nombre positif",
+    "`%s` is unknow"=>"`%s` devrait être inférieur à `%s`",
+    "`%s` should be less than  `%s`"=>"`%s` devrait être inférieur à `%s`",
+    "`%s` should be less than  `%s`"=>"`%s` devrait être inférieur à `%s`",
 ];
