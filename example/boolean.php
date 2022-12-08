68bef97ad151f74bb78f7041e61d6f7a88bd3521
bim-g <ibmussafb@gmail.com>
2022-12-09 01:37:32 +0200

    [UPD] Refactoring boolean example to be mre clear

---
+++ example/boolean.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Copyright (c) 2022.  Wepesi validation.
+ *  @author Boss Ibrahim Mussa
+ */
+$validate = new \Wepesi\App\Validate();
+$schema = new \Wepesi\App\Schema();
+
+$data_source = [
+    'status' => false,
+    'activated' => ""
+];
+//define the schema model for validation
+    $rules=[
+        "status" => $schema->boolean()->required()->max(true)->isValid('TRUE')->generate(),
+        "activated" => $schema->boolean()->required()->min(false)->isValid('FALSE')->generate(),
+    ];
+
+$validate->check($data_source,$rules);
+//    check if the validation passed or not
+    var_dump($validate->passed());
+    var_dump($validate->errors());
