9639f75f68d0d96e4725cfea6d5012e82c137a65
bim-g <ibmussafb@gmail.com>
2022-12-08 23:07:48 +0200

    [UPD] refactor number validation schema and validator example

---
+++ example/index.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Copyright (c) 2022.  Wepesi validation.
+ *  @author Boss Ibrahim Mussa
+ */
+
+use Wepesi\App\Schema;
+
+$data_source = [
+    "name" => "ibrahim",
+    "age" => 12,
+    "country" => "DRC",
+    "state" => "North Kivu",
+    "password"=>"1234567",
+    "new_password"=>123456,
+    "city" => "Goma",
+    "email"=>"infos@wepesi.com",
+    "link"=> "https://github.com/bim-g/wepesi_validation/",
+    "status"=> true,
+    "birth_day"=>"2021-05-23",
+    "date_created"=>"2021-05-23"
+];
+
+//    $valid->check($source,$schema);
+//    include_once __DIR__."/string.php";
+    include __DIR__."/number.php";
+//    include __DIR__."/boolean.php";
+//    include __DIR__."/date.php";
+///
---
+++ example/number.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Copyright (c) 2022.  Wepesi validation.
+ *  @author Boss Ibrahim Mussa
+ */
+
+$validate = new \Wepesi\App\Validate();
+$schema = new \Wepesi\App\Schema();
+$data_source = [
+    "age" => 20,
+    "length" => 0,
+    "height" =>"35",
+    "width" =>"",
+    "direction" => -7
+];
+$rules=[
+    "age" => $schema->number()->min(8)->max(15)->required()->generate(),
+    "length" => $schema->number()->min(1)->max(10)->required()->generate(),
+    "height" => $schema->number()->min(18)->max(50)->required()->generate(),
+    "width" => $schema->number()->min(3)->max(50)->required()->generate(),
+    "direction" => $schema->number()->min(3)->max(50)->positive()->required()->generate(),
+    ];
+$validate->check($data_source,$rules);
+
+var_dump($validate->passed());
+var_dump($validate->errors());
