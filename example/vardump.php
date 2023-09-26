c6c86c4ede60f438ec8febe704bde7668dbd5510
bim-g <ibmussafb@gmail.com>
2023-09-26 10:41:54 +0200

    [FIX] remove unused file

--- example/vardump.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
- * Copyright (c) 2023.  Wepesi validation.
- *  @author Boss Ibrahim Mussa
- */
-var_dump([
-    'passed' => $validate->passed(),
-    'errors' => $validate->errors()
-]);
