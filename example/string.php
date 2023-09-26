8ef3f870e72a0287019d57b47c0f2a805427974d
bim-g <ibmussafb@gmail.com>
2023-09-26 10:42:57 +0200

    [FIX] refactor test string example to get more detail

--- example/string.php
+++ example/string.php
@@ -12,24 +12,44 @@ use Wepesi\App\Validate;
 
 $schema = new Schema();
 $validate = new Validate();
-$source = [
+$source_to_pass = [
+    'name' => 'ibrahim',
+    'country' => null,
+    'password' => '1234567',
+    'new_password' => 1234567,
+    'email' => 'infos@wepesi.com',
+    'link' => 'https://github.com/bim-g/wepesi_validation/',
+    'ip' => '127.0.0.1',
+];
+$source_to_fail = [
     'name' => 'ibrahim',
     'country' => "",
     'password' => '1234567',
     'new_password' => 123456,
-    'email' => 'infos@wepesi.com',
-    'link' => 'https://github.com/bim-g/wepesi_validation/',
+    'email' => 'i@w.com',
+    'link' => 'google',
     'ip' => '192.168',
 ];
 $rules = [
-    "name" => $schema->string()->email()->min(35)->max(50)->required()->generate(),
-    "country" => $schema->string()->min(30)->max(40)->required()->generate(),
-    "password" => $schema->string()->min(3)->max(40)->generate(),
-    "new_password" => $schema->string()->min(3)->max(40)->match("password")->generate(),
-    "email" => $schema->string()->min(3)->max(40)->email()->generate(),
-    "link" => $schema->string()->min(3)->max(40)->url()->generate(),
-    "ip" => $schema->string()->addressIp()->generate()
+    "name" => $schema->string()->min(3)->max(50)->required(),
+    "country" => $schema->any(),
+    "password" => $schema->string()->min(3)->max(40),
+    "new_password" => $schema->string()->min(3)->max(40)->match("password"),
+    "email" => $schema->string()->min(10)->max(40)->email(),
+    "link" => $schema->string()->min(10)->max(45)->url(),
+    "ip" => $schema->string()->addressIp()
 ];
-$validate->check($source, $rules);
-////    check if the validation passed or not
-include_once __DIR__."/vardump.php";
+
+$validate->check($source_to_pass, $rules);
+//  validation pass
+var_dump([
+    'passed' => $validate->passed(),
+    'errors' => $validate->errors()
+]);
+
+//  validation fail
+$validate->check($source_to_fail, $rules);
+var_dump([
+    'passed' => $validate->passed(),
+    'errors' => $validate->errors()
+]);
